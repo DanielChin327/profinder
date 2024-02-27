6f5b5d7d669cb9c34689dcb4c1bec8bbe5b476b8
hiroshi nagao <hi_1205@hotmail.com>
2024-02-27 15:45:47 +0900

    add seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 
 8.times do
   user = User.create!(
-  email: Faker::Internet.safe_email,
+  email: Faker::Internet.email,
   password: '123123'
 )
   gender = 'all'
@@ -22,26 +22,26 @@ legal_service = Service.create(title: "Legal",
   price: ["10000 per a hour", "20000 per a hour", "30000 per a hour"].sample,
   description: "I've been advising and representing clients in criminal or civil proceedings and in other legal matters for more than 20 years in Japan.",
   schedule: "Monday through Friday. 9am to 5pm.",
-  category: ["Attorney", "Law", "Lawyer"]
+  category: ["Attorney", "Law", "Lawyer"].sample
   )
 
 dental_service = Service.create(title: "Tooth Whitening",
   price: ["5000 per a session", "10000 per a session", "20000 per a session"].sample,
   description: "Making your smile ten times brighter.",
   schedule: "Monday through Friday. 9am to 5pm.",
-  category: ["Dentist","Oral Hygene", "Beauty Clinic"]
+  category: ["Dentist","Oral Hygene", "Beauty Clinic"].sample
   )
 
 catering_service = Service.create(title: "Catering",
   price: ["5000 per an event", "10000 per an event", "20000 per an event"].sample,
   description: "Delivering something delicious to everywhere",
   schedule: "Monday through Friday. 9am to 5pm.",
-  category: ["Cooking", "Partying", "Meal Preparation"]
+  category: ["Cooking", "Partying", "Meal Preparation"].sample
   )
 
 real_estae_service = Service.create(title: "Real Estate",
     price: "Free of Charge",
     description: "Introducing your new home in Japan",
     schedule: "Monday through Friday. 9am to 5pm.",
-    category: ["Apartment", "Housing"]
+    category: ["Apartment", "Housing"].sample
     )
