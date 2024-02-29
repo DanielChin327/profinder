cf106c79b325ce26cc0fadc287946841797d2e16
hiroshi nagao <hi_1205@hotmail.com>
2024-02-29 16:43:16 +0900

    add styling to review/reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,6 @@ class Review < ApplicationRecord
   belongs_to :user
   validates :rating, presence: true, numericality: {only_integer: true}, inclusion: { in: [1, 2, 3, 4, 5] }
   validates :comment, presence: true
+  validates :comment, length: { minimum: 5, maximum: 200 }
   validates :eng_rating, presence: true, numericality: {only_integer: true}
 end
