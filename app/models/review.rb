d2856cfcdddd0a39b8f423eae0370e89065e4a92
Daniel Chin <DanChin327@gmail.com>
2024-03-07 17:47:16 +0900

    updated seeds for the demo

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ApplicationRecord
   belongs_to :user
   validates :rating, presence: true, numericality: {only_integer: true}, inclusion: { in: [1, 2, 3, 4, 5] }
   validates :comment, presence: true
-  validates :comment, length: { minimum: 5, maximum: 500 }
+  validates :comment, length: { minimum: 5, maximum: 600 }
   validates :eng_rating, presence: true, numericality: {only_integer: true}, inclusion: { in: [1, 2, 3, 4, 5] }
 end
